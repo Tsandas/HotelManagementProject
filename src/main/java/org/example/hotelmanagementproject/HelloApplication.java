4b4f035601146e0ceee439aaac861e1562853748
Tsandas <georgetsandekidis@gmail.com>
2024-11-06 00:59:39 +0200

    Login stage initial structure

--- src/main/java/org/example/hotelmanagementproject/HelloApplication.java
+++ src/main/java/org/example/hotelmanagementproject/HelloApplication.java
@@ -6,13 +6,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LoginStage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Path cssUrl = Paths.get("src/main/resources/css/LoginStage.css");
+        scene.getStylesheets().add(cssUrl.toUri().toString());
+        stage.setTitle("HotelHub");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/org/example/hotelmanagementproject/LoginController.java
@@ -0,0 +1,9 @@
+package org.example.hotelmanagementproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class LoginController {
+
+
+}
